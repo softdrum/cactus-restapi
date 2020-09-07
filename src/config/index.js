6925bc301adac90c97c1a00d8fd59d0e1459103a
softdrum <drewzerg@gmail.com>
2020-09-07 03:51:18 +0300

    Added mongo database

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 8081,
     db: {
-        url: process.env.DB_URL || 'mongodb://localhost:27017/ConnectionTest'
+        url: process.env.DB_URL || 'mongodb+srv://cactus:cactus1441@cluster0.hgbq7.mongodb.net/<dbname>?retryWrites=true&w=majority'
     },
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const app = require('./app')
 const server = require('http').createServer(app)
 const config = require('./config')
+
+const mongo = require('./database')
+mongo.init()
 /**
  * Main node js server app file
  */
